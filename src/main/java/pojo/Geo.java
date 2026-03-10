bf876d8d4f17c1ca1e7ade67bfc0d94f89cbe23d
Pankaj Nalawade <pankajnl2016@gmail.com>
2026-03-11 00:25:59 +0100

    feature: implement POJO models

---
+++ src/main/java/pojo/Geo.java
@@ -0,0 +1,26 @@
+package pojo;
+
+//Class is used set or get latitute and longitute values and used by Address class
+public class Geo {
+
+    private Double lat;
+    private Double lng;
+
+    public Double getLat() {
+        return lat;
+    }
+
+    public void setLat(Double lat) {
+        this.lat = lat;
+    }
+
+    public Double getLng() {
+        return lng;
+    }
+
+    public void setLng(Double lng) {
+        this.lng = lng;
+    }
+
+
+}
